96dc867fcca1d48c6323687631485712cb8b312f
nikhildike22 <nikhildike22@gmail.com>
2026-08-11 22:59:09 +0530

    changes

--- src/app.js
+++ src/app.js
@@ -1,14 +1,31 @@
-const express = require('express');
-const authRouter = require('./routes/auth.routes')
-const accountRouter = require('../src/routes/account.routes')
-const transactionRoutes = require('./routes/transaction.routes')
-const cookieParser = require('cookie-parser')
-const app = express();
+const express = require("express")
+const cookieParser = require("cookie-parser")
 
-app.use(express.json());
+
+
+const app = express()
+
+
+app.use(express.json())
 app.use(cookieParser())
 
-app.use('/api/auth',authRouter) 
-app.use('/api/account',accountRouter)
-app.use('/api/transactions',transactionRoutes)
-module.exports = app;
+/**
+ * - Routes required
+ */
+const authRouter = require("./routes/auth.routes")
+const accountRouter = require("./routes/account.routes")
+const transactionRoutes = require("./routes/transaction.routes")
+
+/**
+ * - Use Routes
+ */
+
+app.get("/", (req, res) => {
+    res.send("Ledger Service is up and running")
+})
+
+app.use("/api/auth", authRouter)
+app.use("/api/accounts", accountRouter)
+app.use("/api/transactions", transactionRoutes)
+
+module.exports = app
